20bdd85e0554d64c168de81e181baa42a94f99f7
Stephen Senft <stephensenft21@gmail.com>
2019-03-01 13:59:02 -0600

    adding js files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,49 @@
+// Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
+
 // Create an array that contains the words in the sentence
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-function addExcitement (theWordArray) {
+// Add a new argument to the function so that a developer can specify which character should be displayed instead of it always being an exclamation point.
+function addExcitement1 (theWordArray, character, multiplier) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-      buildMeUp += theWordArray[i] + " "
-      console.log(buildMeUp)
       
-        // Print buildMeUp to the console
+      // Add logic to addExcitement that places an exclamation point "!" after every third word. If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
+      if ((i + 1) % 3 === 0 && i !== 0){
+     
+        buildMeUp += theWordArray[i]+ character.repeat(multiplier) + " ";
+       
+      } else{
+        buildMeUp += theWordArray[i] + " ";  
+      }
+      console.log(buildMeUp);
     }
+}
+function addExcitement2 ( theWordArray,character){
+    let buildMeUp = ""
 
+    for (let i = 0; i < theWordArray.length; i++) {
+      
+      // Add logic to addExcitement that places an exclamation point "!" after every third word. If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
+      if ((i + 1) % 3 === 0 && i !== 0){
+     
+        buildMeUp += theWordArray[i]+ character + " ";
+       
+      } else{
+        buildMeUp += theWordArray[i] + " ";  
+      }
+      console.log(buildMeUp);
+    }
 }
 
+
+
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement2(sentence, "?");
+// I want to use a 4 asterisks
+addExcitement1(sentence, "*", 10)
+
+
